cfd2251eb73cbe4c2ea8a7d305def7360427dc7d
John Chadwick <john@jchw.io>
2025-06-03 00:05:54 -0400

    Update to use new API

--- vgm.c
+++ vgm.c
@@ -2,13 +2,14 @@
 // Broken in two parts: need a SF implementation for VGMStream, and a decoder
 // implementation for Deadbeef.
 
+#include <assert.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include <base/plugins.h>
 #include <deadbeef/deadbeef.h>
-#include <streamfile.h>
-#include <vgmstream.h>
+#include <libvgmstream.h>
 
 #include "extensions.h"
 
@@ -32,8 +33,7 @@ static int conf_loop_single = 0;
 static double conf_loop_count = DEFAULT_LOOP_COUNT;
 static double conf_fade_duration = DEFAULT_FADE_DURATION;
 static double conf_fade_delay = DEFAULT_FADE_DELAY;
-static int conf_fade_single = 0;
-static int conf_fade_looping = 1;
+static int conf_force_looping = 0;
 
 static const char settings_dlg[] =
     "property \"Loop count\" entry vgm.loopcount " DEFAULT_LOOP_COUNT_STR ";\n"
@@ -41,103 +41,134 @@ static const char settings_dlg[] =
     "vgm.fadeduration " DEFAULT_FADE_DURATION_STR ";\n"
     "property \"Fade delay (seconds)\" entry "
     "vgm.fadedelay " DEFAULT_FADE_DELAY_STR ";\n"
-    "property \"Enable fade (single-shot)\" checkbox vgm.fadesingle 0;\n"
-    "property \"Enable fade (looping)\" checkbox vgm.fadelooping 1;\n";
+    "property \"Force looping\" checkbox vgm.forcelooping 0;\n";
 
 /*
  * VGMStream Streamfile implementation for Deadbeef
  */
 
-typedef struct _DBSTREAMFILE {
-  STREAMFILE sf;
+typedef struct {
   DB_FILE *file;
   off_t offset;
-  char name[260];
-} DBSTREAMFILE;
+  char *name;
+} db_priv_t;
 
-static void dbsf_seek(DBSTREAMFILE *this, off_t offset) {
-  if (!this->file) {
+static void dbsf_seek(void *user_data, off_t offset) {
+  db_priv_t *priv = (db_priv_t *)user_data;
+  if (!priv || !priv->file) {
     return;
   }
-  if (deadbeef->fseek(this->file, offset, SEEK_SET) == 0) {
-    this->offset = offset;
+  if (deadbeef->fseek(priv->file, offset, SEEK_SET) == 0) {
+    priv->offset = offset;
   } else {
-    this->offset = deadbeef->ftell(this->file);
+    priv->offset = deadbeef->ftell(priv->file);
   }
 }
 
-static off_t dbsf_get_size(DBSTREAMFILE *this) {
-  if (!this->file) {
+static int64_t dbsf_get_size(void *user_data) {
+  db_priv_t *priv = (db_priv_t *)user_data;
+  if (!priv || !priv->file) {
     return 0;
   }
-  return deadbeef->fgetlength(this->file);
+  return deadbeef->fgetlength(priv->file);
 }
 
-static off_t dbsf_get_offset(DBSTREAMFILE *this) { return this->offset; }
-
-static void dbsf_get_name(DBSTREAMFILE *this, char *buffer, size_t length) {
-  strncpy(buffer, this->name, length);
-  buffer[length - 1] = '\0';
-}
-
-static size_t dbsf_read(DBSTREAMFILE *this, uint8_t *dest, off_t offset,
-                        size_t length) {
-  size_t read;
-  if (!this->file) {
+static off_t dbsf_get_offset(void *user_data) {
+  db_priv_t *priv = (db_priv_t *)user_data;
+  if (!priv) {
     return 0;
   }
-  if (this->offset != offset) {
-    dbsf_seek(this, offset);
-  }
-  read = deadbeef->fread(dest, 1, length, this->file);
-  if (read > 0) {
-    this->offset += read;
-  }
-  return read;
+  return priv->offset;
 }
 
-static void dbsf_close(DBSTREAMFILE *this) {
-  if (this->file) {
-    deadbeef->fclose(this->file);
-    this->file = NULL;
-  }
-  free(this);
-}
-
-static STREAMFILE *dbsf_create_from_path(const char *path);
-static STREAMFILE *dbsf_open(DBSTREAMFILE *this, const char *const filename,
-                             size_t buffersize) {
-  if (!filename)
+static const char *dbsf_get_name(void *user_data) {
+  db_priv_t *priv = (db_priv_t *)user_data;
+  if (!priv) {
     return NULL;
+  }
+  return priv->name;
+}
+
+static int dbsf_read(void *user_data, uint8_t *dst, int64_t offset,
+                     int length) {
+  db_priv_t *priv = (db_priv_t *)user_data;
+  size_t read;
+  if (!priv || !priv->file) {
+    return 0;
+  }
+  if (priv->offset != offset) {
+    dbsf_seek(priv, offset);
+  }
+  read = deadbeef->fread(dst, 1, length, priv->file);
+  if (read > 0) {
+    priv->offset += read;
+  }
+  assert(read <= INT_MAX);
+  return (int)read;
+}
+
+static void dbsf_close(struct libstreamfile_t *libsf) {
+  if (!libsf) {
+    return;
+  }
+
+  db_priv_t *priv = (db_priv_t *)libsf->user_data;
+  if (priv && priv->file) {
+    deadbeef->fclose(priv->file);
+    priv->file = NULL;
+  }
+
+  free(priv);
+  free(libsf);
+}
+
+static libstreamfile_t *dbsf_create_from_path(const char *path);
+static libstreamfile_t *dbsf_open(void *user_data, const char *const filename) {
+  if (!filename) {
+    return NULL;
+  }
   return dbsf_create_from_path(filename);
 }
 
-static STREAMFILE *dbsf_create(DB_FILE *file, const char *path) {
-  DBSTREAMFILE *streamfile = malloc(sizeof(DBSTREAMFILE));
+static libstreamfile_t *dbsf_create(DB_FILE *file, const char *path) {
+  libstreamfile_t *libsf = NULL;
+  db_priv_t *priv = NULL;
 
-  if (!streamfile)
-    return NULL;
+  libsf = (libstreamfile_t *)calloc(1, sizeof(libstreamfile_t));
+  if (!libsf) {
+    goto err1;
+  }
 
-  memset(streamfile, 0, sizeof(DBSTREAMFILE));
-  streamfile->sf.read = (void *)dbsf_read;
-  streamfile->sf.get_size = (void *)dbsf_get_size;
-  streamfile->sf.get_offset = (void *)dbsf_get_offset;
-  streamfile->sf.get_name = (void *)dbsf_get_name;
-  streamfile->sf.open = (void *)dbsf_open;
-  streamfile->sf.close = (void *)dbsf_close;
-  streamfile->file = file;
-  streamfile->offset = 0;
-  strncpy(streamfile->name, path, sizeof(streamfile->name));
+  libsf->read = dbsf_read;
+  libsf->get_size = dbsf_get_size;
+  libsf->get_name = dbsf_get_name;
+  libsf->open = dbsf_open;
+  libsf->close = dbsf_close;
 
-  return &streamfile->sf;
+  priv = (db_priv_t *)calloc(1, sizeof(db_priv_t));
+  if (!priv) {
+    goto err1;
+  }
+
+  priv->file = file;
+  priv->offset = 0;
+  priv->name = strdup(path);
+  libsf->user_data = priv;
+
+  return libsf;
+
+err1:
+  free(libsf);
+  free(priv);
+  return NULL;
 }
 
-STREAMFILE *dbsf_create_from_path(const char *path) {
+static libstreamfile_t *dbsf_create_from_path(const char *path) {
   DB_FILE *file = deadbeef->fopen(path);
 
   if (!file) {
     /* Allow vgmstream's virtual files. */
-    if (!vgmstream_is_virtual_filename(path)) {
+    if (!libvgmstream_is_virtual_filename(path)) {
       return NULL;
     }
   }
@@ -145,24 +176,29 @@ STREAMFILE *dbsf_create_from_path(const char *path) {
   return dbsf_create(file, path);
 }
 
-VGMSTREAM *init_vgmstream_from_dbfile(const char *path, int subsong) {
-  STREAMFILE *sf;
-  VGMSTREAM *vgm;
+static libvgmstream_t *init_vgmstream_from_dbfile(const char *path, int subsong) {
+  libstreamfile_t *sf = NULL;
+  libvgmstream_t *vgmstream = NULL;
 
   sf = dbsf_create_from_path(path);
-  if (!sf)
+  if (!sf) {
     return NULL;
+  }
 
-  sf->stream_index = subsong;
+  libvgmstream_config_t vcfg;
+  memset(&vcfg, 0, sizeof(libvgmstream_config_t));
+  vcfg.allow_play_forever = conf_loop_single;
+  vcfg.play_forever = conf_loop_single;
+  vcfg.loop_count = conf_loop_count;
+  vcfg.fade_time = conf_fade_duration;
+  vcfg.fade_delay = conf_fade_delay;
+  vcfg.force_loop = conf_force_looping;
 
-  vgm = init_vgmstream_from_STREAMFILE(sf);
-  if (!vgm)
-    goto err1;
-
-  return vgm;
-err1:
-  dbsf_close((DBSTREAMFILE *)sf);
-  return NULL;
+  // Believe it or not, this is correct: we don't need to do anything different
+  // in the error case versus the success case.
+  vgmstream = libvgmstream_create(sf, subsong, &vcfg);
+  libstreamfile_close(sf);
+  return vgmstream;
 }
 
 /*
@@ -171,10 +207,9 @@ err1:
 
 typedef struct {
   DB_fileinfo_t info;
-  VGMSTREAM *s;
+  libvgmstream_t *s;
   int can_loop;
   int position;
-  int fadesamples;
   int totalsamples;
 } vgm_info_t;
 
@@ -228,17 +263,31 @@ static int vgm_init(DB_fileinfo_t *_info, DB_playItem_t *it) {
     return -1;
   }
 
-  info->fadesamples = conf_fade_duration * info->s->sample_rate;
-  info->totalsamples = get_vgmstream_play_samples(
-      conf_loop_count, conf_fade_duration, conf_fade_delay, info->s);
+  info->totalsamples = info->s->format->play_samples;
 
   _info->readpos = 0;
   _info->plugin = &plugin;
   _info->fmt.bps = 16;
-  _info->fmt.channels = info->s->channels;
-  _info->fmt.samplerate = info->s->sample_rate;
+  _info->fmt.channels = info->s->format->channels;
+  _info->fmt.samplerate = info->s->format->sample_rate;
 
-  int i;
+  deadbeef->pl_lock();
+  int i, num_playlists = deadbeef->plt_get_count();
+  // Unfortunately, there's no way to get the playlist directly from the
+  // DB_playItem_t. Loop through all playlists and find the right one.
+  for (i = 0; i < num_playlists; i++) {
+    ddb_playlist_t *plt = deadbeef->plt_get_for_idx(i);
+    if (!plt || deadbeef->plt_get_item_idx(plt, it, 0) == -1) {
+      deadbeef->plt_unref(plt);
+      continue;
+    }
+    deadbeef->plt_set_item_duration(plt, it,
+                                    (float)info->totalsamples / _info->fmt.samplerate);
+    deadbeef->plt_unref(plt);
+    break;
+  }
+  deadbeef->pl_unlock();
+
   for (i = 0; i < _info->fmt.channels; i++) {
     _info->fmt.channelmask |= 1 << i;
   }
@@ -248,92 +297,42 @@ static int vgm_init(DB_fileinfo_t *_info, DB_playItem_t *it) {
 
 static void vgm_free(DB_fileinfo_t *_info) {
   vgm_info_t *info = (vgm_info_t *)_info;
-  close_vgmstream(info->s);
+  libvgmstream_free(info->s);
   if (info) {
     free(info);
   }
 }
 
 static int vgm_read(DB_fileinfo_t *_info, char *bytes, int size) {
-  int i, j;
   vgm_info_t *info = (vgm_info_t *)_info;
   int sample_size = _info->fmt.channels * sizeof(int16_t);
-  int32_t sample_count = size / sample_size,
-          fade_start = info->totalsamples - info->fadesamples,
-          fade_end = info->totalsamples, fade_samples = info->fadesamples;
+  int32_t sample_count = size / sample_size;
 
-  int terminate = !conf_loop_single || !info->can_loop || !info->s->loop_flag;
-  int is_looping = info->s->loop_flag;
-  int do_fade = ((conf_fade_single && !is_looping) ||
-                (conf_fade_looping && is_looping)) && terminate;
-
-  if (terminate) {
-    /* Past the end? Let deadbeef know. */
-    if (info->position >= info->totalsamples) {
-      return 0;
-    }
+  if (info->s->decoder->done) {
+    /* At the end? Let deadbeef know. */
+    return 0;
   }
 
   /* Do the actual rendering. */
-  sample_count = render_vgmstream((int16_t *)bytes, sample_count, info->s);
+  int result = libvgmstream_fill(info->s, bytes, sample_count);
+  if (result < 0) {
+    /* An error occurred. Signal EOF. */
+    return 0;
+  }
+  sample_count = info->s->decoder->buf_samples;
 
   /* Update position. */
   info->position += sample_count;
   _info->readpos = (float)info->position / (float)_info->fmt.samplerate;
 
-  /* If we are overlapping any piece after the fade starts, we must fade out
-   * here.
-   * TODO: Code should be refactored to be more clear.
-   */
-  if (do_fade && (info->position > fade_start ||
-                  info->position + sample_count > fade_start)) {
-    int16_t *buf = (int16_t *)bytes;
-    for (i = 0; i < sample_count; ++i) {
-      int pos = i + info->position;
-      if (pos > fade_start) {
-        int samples_into_fade = pos - fade_start;
-        double fadedness =
-            (double)(fade_samples - samples_into_fade) / fade_samples;
-        for (j = 0; j < info->s->channels; j++) {
-          buf[i * info->s->channels + j] =
-              buf[i * info->s->channels + j] * fadedness;
-        }
-      } else if (pos > fade_end) {
-        break;
-      }
-    }
-  }
-
   return sample_count * sample_size;
 }
 
 static int vgm_seek_sample(DB_fileinfo_t *_info, int sample) {
-  int i;
   vgm_info_t *info = (vgm_info_t *)_info;
-  int16_t samples[0x1000 * _info->fmt.channels];
-
-  /* Reset stream to go backwards */
-  if (sample < info->position) {
-    reset_vgmstream(info->s);
-    info->position = 0;
-    _info->readpos = 0;
-  }
-
-  int seek = sample - info->position;
-
-  /* Render out 0x1000 sample chunks and discard them */
-  while (seek > 0x1000) {
-    vgm_read(_info, (char *)samples,
-             0x1000 * _info->fmt.channels * sizeof(int16_t));
-    seek -= 0x1000;
-  }
-
-  /* Read the final chunk of < 0x1000 samples (or not) */
-  if (seek > 0) {
-    vgm_read(_info, (char *)samples,
-             seek * _info->fmt.channels * sizeof(int16_t));
-  }
-
+  libvgmstream_seek(info->s, sample);
+  info->position = sample;
+  _info->readpos = (float)info->position / (float)_info->fmt.samplerate;
   return 0;
 }
 
@@ -344,68 +343,68 @@ static int vgm_seek(DB_fileinfo_t *_info, float time) {
 static DB_playItem_t *vgm_insert_subsong(ddb_playlist_t *plt,
                                          DB_playItem_t *after,
                                          const char *fname, int subsong) {
-  VGMSTREAM *vgm = init_vgmstream_from_dbfile(fname, subsong);
+  libvgmstream_t *vgm = init_vgmstream_from_dbfile(fname, subsong);
   if (!vgm) {
     return after;
   }
 
   DB_playItem_t *it = deadbeef->pl_item_alloc_init(fname, plugin.plugin.id);
 
-  vgmstream_title_t tcfg;
-  memset(&tcfg, 0, sizeof(vgmstream_title_t));
+  libvgmstream_title_t tcfg;
+  memset(&tcfg, 0, sizeof(libvgmstream_title_t));
   tcfg.remove_extension = 1;
+  tcfg.remove_archive = 1;
+  tcfg.filename = fname;
 
   char title[1024];
-
-  vgmstream_get_title(title, sizeof(title), fname, vgm, &tcfg);
+  libvgmstream_get_title(vgm, &tcfg, title, sizeof(title));
   deadbeef->pl_add_meta(it, "title", title);
   deadbeef->pl_replace_meta(it, ":FILETYPE", "vgm");
   deadbeef->pl_set_meta_int(it, ":TRACKNUM", subsong);
-  if (vgm->loop_flag) {
-    deadbeef->pl_set_meta_int(it, ":loop_start", vgm->loop_start_sample);
-    deadbeef->pl_set_meta_int(it, ":loop_end", vgm->loop_end_sample);
+  if (vgm->format->loop_flag) {
+    deadbeef->pl_set_meta_int(it, ":loop_start", vgm->format->loop_start);
+    deadbeef->pl_set_meta_int(it, ":loop_end", vgm->format->loop_end);
   }
-  if (vgm->num_streams > 1) {
-    deadbeef->pl_set_meta_int(it, ":stream_count", vgm->num_streams);
+  if (vgm->format->subsong_count > 1) {
+    deadbeef->pl_set_meta_int(it, ":stream_count", vgm->format->subsong_count);
   }
-  if (vgm->stream_index > 1) {
-    deadbeef->pl_set_meta_int(it, ":stream_index", vgm->stream_index);
+  if (vgm->format->subsong_index > 1) {
+    deadbeef->pl_set_meta_int(it, ":stream_index", vgm->format->subsong_index);
   }
-  if (vgm->stream_name != NULL && *vgm->stream_name != '\0') {
-    deadbeef->pl_add_meta(it, ":stream_name", vgm->stream_name);
+  if (vgm->format->stream_name[0] != 0) {
+    deadbeef->pl_add_meta(it, ":stream_name", vgm->format->stream_name);
   }
-  deadbeef->pl_set_meta_int(it, ":SAMPLERATE", vgm->sample_rate);
-  deadbeef->pl_set_meta_int(it, ":CHANNELS", vgm->channels);
+  deadbeef->pl_set_meta_int(it, ":SAMPLERATE", vgm->format->sample_rate);
+  deadbeef->pl_set_meta_int(it, ":CHANNELS", vgm->format->channels);
   deadbeef->pl_set_meta_int(it, ":BPS", 16);
   deadbeef->pl_set_meta_int(it, ":BITRATE",
-                            get_vgmstream_average_bitrate(vgm) / 1000);
+                            vgm->format->stream_bitrate / 1000);
 
-  size_t num_samples = get_vgmstream_play_samples(
-      conf_loop_count, conf_fade_duration, conf_fade_delay, vgm);
+  size_t num_samples = vgm->format->play_samples;
   deadbeef->plt_set_item_duration(plt, it,
-                                  (float)num_samples / vgm->sample_rate);
+                                  (float)num_samples / vgm->format->sample_rate);
 
   after = deadbeef->plt_insert_item(plt, after, it);
   deadbeef->pl_item_unref(it);
 
-  close_vgmstream(vgm);
+  libvgmstream_free(vgm);
 
   return after;
 }
 
 static DB_playItem_t *vgm_insert(ddb_playlist_t *plt, DB_playItem_t *after,
                                  const char *fname) {
-  VGMSTREAM *vgm = init_vgmstream_from_dbfile(fname, 0);
+  libvgmstream_t *vgm = init_vgmstream_from_dbfile(fname, 0);
   if (vgm == NULL) {
     return after;
   }
 
-  int i, num_subsongs = vgm->num_streams > 0 ? vgm->num_streams : 1;
-  int add_subsong = (num_subsongs > 1 && vgm->stream_index == 0)
+  int i, num_subsongs = vgm->format->subsong_count > 0 ? vgm->format->subsong_count : 1;
+  int add_subsong = (num_subsongs > 1 && vgm->format->subsong_index == 0)
                         ? -1
-                        : (vgm->stream_index || 1);
+                        : (vgm->format->subsong_index || 1);
 
-  close_vgmstream(vgm);
+  libvgmstream_free(vgm);
 
   if (add_subsong == -1) {
     for (i = 1; i <= num_subsongs; ++i) {
@@ -420,16 +419,15 @@ static DB_playItem_t *vgm_insert(ddb_playlist_t *plt, DB_playItem_t *after,
 
 static void vgm_reload_config(void) {
   conf_loop_single =
-      deadbeef->conf_get_int("playback.loop", PLAYBACK_MODE_LOOP_ALL) ==
-      PLAYBACK_MODE_LOOP_SINGLE;
+      deadbeef->conf_get_int("playback.loop", DDB_REPEAT_ALL) ==
+      DDB_REPEAT_SINGLE;
   conf_loop_count = (double)deadbeef->conf_get_float("vgm.loopcount",
                                                      (float)DEFAULT_LOOP_COUNT);
   conf_fade_duration = (double)deadbeef->conf_get_float(
       "vgm.fadeduration", (float)DEFAULT_FADE_DURATION);
   conf_fade_delay = (double)deadbeef->conf_get_float("vgm.fadedelay",
                                                      (float)DEFAULT_FADE_DELAY);
-  conf_fade_single = deadbeef->conf_get_int("vgm.fadesingle", 0);
-  conf_fade_looping = deadbeef->conf_get_int("vgm.fadelooping", 1);
+  conf_force_looping = deadbeef->conf_get_int("vgm.forcelooping", 0);
 }
 
 static int vgm_start(void) {
